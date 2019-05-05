33c2d29cf09477cbf9a9e86445edd1da645c3562
nicomelnyk <nicolasmelnyk@hotmail.com>
2019-05-05 19:17:11 -0300

    Se comenzo a estructurar el main

--- Lissandra/src/memoria/memoria-main.c
+++ Lissandra/src/memoria/memoria-main.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <commons/collections/list.h>
+#include <commons/log.h>
+
+#include "../commons/lissandra-threads.h"
+#include "memoria-logger.h"
+#include "memoria-config.h"
+#include "memoria-main.h"
+
+pthread_mutex_t memoria_main_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t memoria_main_cond = PTHREAD_COND_INITIALIZER;
+
+/*
+void inicializar_memoria() {
+
+}
+
+void liberar_recursos_memoria() {
+
+}
+*/
+
+void finalizar_memoria(){
+
+}
 
 int main() {
-	printf("Memoria main!\n");
+	//inicializar_memoria();
+
+	// inicializo hilos
+
+	pthread_mutex_lock(&memoria_main_mutex);
+	pthread_cond_wait(&memoria_main_cond, &memoria_main_mutex);
+	pthread_mutex_unlock(&memoria_main_mutex);
+
+	// finalizar hilos
+
+	//liberar_recursos_memoria();
+	return 0;
 }
---
+++ Lissandra/src/memoria/memoria-main.h
@@ -0,0 +1,6 @@
+#ifndef LISSANDRA_SRC_MEMORIA_MEMORIA_MAIN_H_
+#define LISSANDRA_SRC_MEMORIA_MEMORIA_MAIN_H_
+
+void finalizar_memoria();
+
+#endif
