994d97df81371767e64590efeaadf08d34da6e11
PabloPaglilla <pablopaglilla16@gmail.com>
2019-05-12 22:11:30 -0300

    Actualizar módulo kernel-config
    Se agregó al archivo de configuración el número de la memoria cuya IP y
    puerto se encuentran en la configuración. También se modificó el nombre
    de la variable t_config* a 'kernel_configuracion' para evitar colisiones
    con otros módulos.

--- Lissandra/src/kernel/kernel-config.c
+++ Lissandra/src/kernel/kernel-config.c
@@ -11,21 +11,22 @@
 // Claves del archivo de configuración
 #define IP_MEMORIA "IP_MEMORIA"
 #define PUERTO_MEMORIA "PUERTO_MEMORIA"
+#define NUMERO_MEMORIA "NUMERO_MEMORIA"
 #define QUANTUM "QUANTUM"
 #define MULTIPROCESAMIENTO "MULTIPROCESAMIENTO"
 #define METADATA_REFRESH "METADATA_REFRESH"
 #define MEMORIAS_REFRESH "MEMORIAS_REFRESH"
 #define SLEEP_EJECUCION "SLEEP_EJECUCION"
 
-t_config* configuracion;
+t_config* kernel_configuracion;
 pthread_rwlock_t kernel_config_lock = PTHREAD_RWLOCK_INITIALIZER;
 
 int inicializar_kernel_config() {
-	return inicializar_configuracion(&configuracion, CONFIG_PATH, &kernel_config_lock);
+	return inicializar_configuracion(&kernel_configuracion, CONFIG_PATH, &kernel_config_lock);
 }
 
 int actualizar_kernel_config() {
-	return actualizar_configuracion(&configuracion, CONFIG_PATH, &kernel_config_lock);
+	return actualizar_configuracion(&kernel_configuracion, CONFIG_PATH, &kernel_config_lock);
 }
 
 int inicializar_kernel_inotify(lissandra_thread_t* l_thread) {
@@ -33,33 +34,37 @@ int inicializar_kernel_inotify(lissandra_thread_t* l_thread) {
 }
 
 int destruir_kernel_config() {
-	return destruir_configuracion(configuracion, &kernel_config_lock);
+	return destruir_configuracion(kernel_configuracion, &kernel_config_lock);
 }
 
 char* get_ip_memoria() {
-	return get_string_value(configuracion, IP_MEMORIA, &kernel_config_lock);
+	return get_string_value(kernel_configuracion, IP_MEMORIA, &kernel_config_lock);
 }
 
 int get_puerto_memoria() {
-	return get_int_value(configuracion, PUERTO_MEMORIA, &kernel_config_lock);
+	return get_int_value(kernel_configuracion, PUERTO_MEMORIA, &kernel_config_lock);
+}
+
+int get_numero_memoria() {
+	return get_int_value(kernel_configuracion, NUMERO_MEMORIA, &kernel_config_lock);
 }
 
 int get_quantum() {
-	return get_int_value(configuracion, QUANTUM, &kernel_config_lock);
+	return get_int_value(kernel_configuracion, QUANTUM, &kernel_config_lock);
 }
 
 int get_multiprocesamiento() {
-	return get_int_value(configuracion, MULTIPROCESAMIENTO, &kernel_config_lock);
+	return get_int_value(kernel_configuracion, MULTIPROCESAMIENTO, &kernel_config_lock);
 }
 
 int get_refresh_metadata() {
-	return get_int_value(configuracion, METADATA_REFRESH, &kernel_config_lock);
+	return get_int_value(kernel_configuracion, METADATA_REFRESH, &kernel_config_lock);
 }
 
 int get_refresh_memorias() {
-	return get_int_value(configuracion, MEMORIAS_REFRESH, &kernel_config_lock);
+	return get_int_value(kernel_configuracion, MEMORIAS_REFRESH, &kernel_config_lock);
 }
 
 int get_retardo_ejecucion() {
-	return get_int_value(configuracion, SLEEP_EJECUCION, &kernel_config_lock);
+	return get_int_value(kernel_configuracion, SLEEP_EJECUCION, &kernel_config_lock);
 }
--- Lissandra/src/kernel/kernel-config.h
+++ Lissandra/src/kernel/kernel-config.h
@@ -10,6 +10,7 @@ int destruir_kernel_config();
 
 char* get_ip_memoria();
 int get_puerto_memoria();
+int get_numero_memoria();
 int get_quantum();
 int get_multiprocesamiento();
 int get_refresh_metadata();
