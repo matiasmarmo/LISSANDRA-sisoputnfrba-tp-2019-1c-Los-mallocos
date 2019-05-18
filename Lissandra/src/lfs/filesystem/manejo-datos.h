6eb3a632b87674be171170c0b5fcc2968cfa1802
Pablo Paglilla <pablopaglilla16@gmail.com>
2019-05-18 20:02:24 -0300

    Agregar declaraciones de funciones de apertura a manejo-datos.h

--- Lissandra/src/lfs/filesystem/manejo-datos.h
+++ Lissandra/src/lfs/filesystem/manejo-datos.h
@@ -1,7 +1,15 @@
 #ifndef MANEJO_DATOS_H_
 #define MANEJO_DATOS_H_
 
+#include <stdio.h>
+
 int path_a_bloque(char *numero_bloque, char *buffer, int tamanio_buffer);
+
+FILE *abrir_archivo_para_lectura(char *path);
+
+FILE *abrir_archivo_para_escritura(char *path);
+
+FILE *abrir_archivo_para_agregar(char *path);
 
 int iterar_archivo_de_datos(char *path, operacion_t operacion);
 
