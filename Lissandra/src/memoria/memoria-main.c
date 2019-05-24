8c8da8e5c26960d6587b58f954acc49ddeb42a58
nicomelnyk <nicolasmelnyk@hotmail.com>
2019-05-23 21:00:10 -0300

    Se avanza con el servidor. Principio de comunicacion con el kernel

--- Lissandra/src/memoria/memoria-main.c
+++ Lissandra/src/memoria/memoria-main.c
@@ -10,6 +10,7 @@
 #include "../commons/lissandra-threads.h"
 #include "memoria-logger.h"
 #include "memoria-config.h"
+#include "memoria-server.h"
 #include "memoria-main.h"
 
 struct segmento{
@@ -49,10 +50,21 @@ void finalizar_memoria(){
 int main() {
 	//inicializar_memoria();
 	int tamanio_memoria = get_tamanio_memoria();
-	char* memoria = calloc(tamanio_memoria, sizeof(char));
+	//char* memoria = calloc(tamanio_memoria, sizeof(char));
 
 	t_list* TABLA_DE_SEGMENTOS = list_create();
 	t_list* TABLA_DE_PAGINAS = list_create();
+
+	inicializar_memoria_logger();
+	inicializar_memoria_config();
+	lissandra_thread_t l_thread;
+	l_thread_create(&l_thread, &correr_servidor_memoria, NULL);
+	sleep(1000000);
+	l_thread_solicitar_finalizacion(&l_thread);
+	l_thread_join(&l_thread, NULL);
+	destruir_memoria_logger();
+	destruir_memoria_config();
+	return 0;
 
 	// inicializo hilos
 
--- Lissandra/src/memoria/memoria-server.c
+++ Lissandra/src/memoria/memoria-server.c
@@ -15,6 +15,7 @@
 #include "../commons/consola/consola.h"
 #include "../commons/lissandra-threads.h"
 #include "../commons/consola/consola.h"
+#include "../commons/comunicacion/protocol-utils.h"
 #include "memoria-logger.h"
 #include "memoria-config.h"
 #include "memoria-main.h"
@@ -40,37 +41,55 @@ void manejarCliente(int cliente, int* posicion){
 	int error;
 	int tamanio_buffers = get_max_msg_size();
 	uint8_t buffer[tamanio_buffers], respuesta[tamanio_buffers];
+	memset(buffer, 0, tamanio_buffers);
+	memset(respuesta, 0, tamanio_buffers);
 	if ((error = recv_msg(cliente, buffer, tamanio_buffers) < 0)) {
 		memoria_log_to_level(LOG_LEVEL_TRACE, false,
 				"Fallo al recibir el mensaje del cliente");
-		if (error == SOCKET_ERROR) {
-			//break;
-		} if (error == CONN_CLOSED) {
+		if (error == SOCKET_ERROR || error == CONN_CLOSED) {
 			sacarCliente(cliente, posicion);
-		} else {
-			//continue;
+			close(cliente);
 		}
+		return;
 	}
+	imprimir_async("Se envia respuesta al kernel");
+	char *ips[2] = { "192.168.1.8", "192.168.1.8"};
+	uint32_t ips_ints[2];
+	for(int i = 0; i < 2; i++) {
+		ipv4_a_uint32(ips[i], &(ips_ints[i]));
+	}
+	uint16_t puertos[2] = {1,2};
+	uint8_t numeros[2] = {1,2};
+	if(get_msg_id(buffer) == DESCRIBE_REQUEST_ID) {
+		printf("Describe\n");
+		send_global_describe_response(0, "TablaA;TablaB", 2, puertos, 2, numeros, 2, ips_ints, cliente);
+	} else if (get_msg_id(buffer) == GOSSIP_ID) {
+		printf("GOSSIP\n");
+		send_gossip_response(2, ips_ints, 2, puertos, 2, numeros, cliente);
+	} else {
+		printf("Otro\n");
+		send_drop_response(0, "Tabla", cliente);
+	}
+
+	return;
+	// ejecutar_request(buffer, respuesta);
 	destroy(buffer);
 	if ((error = send_msg(cliente, respuesta) < 0)) { // intento reconectarme
 		memoria_log_to_level(LOG_LEVEL_TRACE, false,
 				"Fallo al enviar el mensaje al cliente");
-		if (error == SOCKET_ERROR) {
-			//break;
-		} if (error == CONN_CLOSED) {
-			//sacarCliente(cliente, posicion);
-		} else {
-			//continue;
+		if (error == SOCKET_ERROR || error == CONN_CLOSED) {
+			sacarCliente(cliente, posicion);
+			close(cliente);
 		}
+		destroy(respuesta);
 	}
-	memset(buffer, 0, tamanio_buffers);
-	close(cliente);
+	destroy(respuesta);
 }
 
-void agregarCliente(int* nuevo_cliente) {
+void agregarCliente(int nuevo_cliente) {
 	if (cant_actual < 2){
 		clientes[cant_actual] = nuevo_cliente;
-		cant_actual ++;
+		cant_actual++;
 	} else {
 		memoria_log_to_level(LOG_LEVEL_TRACE, false,
 				"Se intentó conectar un tercer cliente en la memoria");
@@ -95,9 +114,13 @@ void manejar_consola_memoria(char* linea, void* request) {
 	if (get_msg_id(request) == EXIT_REQUEST_ID) {
 		finalizar_memoria();
 	}
-	//llamar a lissandra(request)
-	//manejar error (si tira error)
+	int tamanio_buffers = get_max_msg_size();
+	uint8_t respuesta[tamanio_buffers];
+	memset(respuesta, 0, tamanio_buffers);
+	// ejecutar_request(request, respuesta);
+	mostrar(respuesta);
 	destroy(request);
+	destroy(respuesta);
 }
 
 void* correr_servidor_memoria(void* entrada) {
@@ -111,7 +134,7 @@ void* correr_servidor_memoria(void* entrada) {
 		pthread_exit(NULL);
 	}
 
-	//iniciar_consola(&manejar_consola);
+	iniciar_consola(&manejar_consola_memoria);
 
 	fd_set descriptores, copia;
 	FD_ZERO(&descriptores);
@@ -119,41 +142,39 @@ void* correr_servidor_memoria(void* entrada) {
 	FD_SET(STDIN_FILENO, &descriptores);
 
 	int i;
-	int *nuevo_cliente;
+	int nuevo_cliente;
 	int select_ret;
+	int mayor_file_descriptor = servidor;
 	struct timespec ts;
 	ts.tv_sec = 0;
 	ts.tv_nsec = 250000000;
 
 	while (!l_thread_debe_finalizar(l_thread)) {
 		copia = descriptores;
-		select_ret = pselect(servidor + 1, &copia, NULL, NULL, &ts, NULL);
+		select_ret = pselect(mayor_file_descriptor + 1, &copia, NULL, NULL, &ts, NULL);
 		if (select_ret == -1) {
 			memoria_log_to_level(LOG_LEVEL_TRACE, false,
 					"Fallo en la ejecucion del select del servidor");
 			break;
 		} else if (select_ret > 0) {
 			if (FD_ISSET(servidor, &copia)) {
-				*nuevo_cliente = aceptar_cliente(servidor);
-				if(nuevo_cliente >= 0) {
+				nuevo_cliente = aceptar_cliente(servidor);
+				if(nuevo_cliente > 0) {
 					agregarCliente(nuevo_cliente);
+					if(nuevo_cliente > mayor_file_descriptor) {
+						mayor_file_descriptor = nuevo_cliente;
+					}
 					FD_SET(clientes[cant_actual-1], &descriptores);
 				}
 			}
 			if (FD_ISSET(STDIN_FILENO, &copia)) {
 				leer_siguiente_caracter();
-				*nuevo_cliente = aceptar_cliente(servidor);
-				if(nuevo_cliente >= 0) {
-					agregarCliente(nuevo_cliente);
-					FD_SET(clientes[cant_actual-1], &descriptores);
-				}
 			}
 			for(i=0; i<cant_actual ; i++) {
-				if(clientes[i] != -1) {
+				if(clientes[i] != -1 && FD_ISSET(clientes[i], &copia)) {
 					manejarCliente(clientes[i], &i);
 				}
 			}
-			free(nuevo_cliente);
 		}
 	}
 	cerrar_consola();
