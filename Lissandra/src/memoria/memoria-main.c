9f626de9055d9ec5d81e57eab738ee147338f29d
nicomelnyk <nicolasmelnyk@hotmail.com>
2019-06-19 00:22:16 -0300

    Se agrega funcion para inicializar toda la memoria

--- Lissandra/src/memoria/memoria-main.c
+++ Lissandra/src/memoria/memoria-main.c
@@ -22,9 +22,18 @@ pthread_cond_t memoria_main_cond = PTHREAD_COND_INITIALIZER;
 
 int tamanio_maximo_value;
 
+void inicializar_memoria(){
+	inicializar_memoria_config();
+	inicializar_memoria_logger();
+	inicializar_clientes();
+	inicializacion_memoria();
+	inicializacion_tabla_segmentos();
+}
+
 int get_tamanio_maximo_pagina() {
 	return sizeof(uint16_t) + sizeof(uint64_t) + tamanio_maximo_value + 1; //BYTES
 }
+
 void finalizar_memoria() {
 	pthread_mutex_lock(&memoria_main_mutex);
 	pthread_cond_signal(&memoria_main_cond);
@@ -32,13 +41,10 @@ void finalizar_memoria() {
 }
 
 int main() {
-	//inicializar_memoria();
-	inicializar_memoria_config();
-	inicializar_memoria_logger();
-	inicializar_clientes();
-	tamanio_maximo_value = 15; // PEDIRSELO AL FS (BYTES)
-	inicializacion_memoria();
-	inicializacion_tabla_segmentos();
+	inicializar_memoria();
+
+	tamanio_maximo_value = 15; // PEDIRSELO AL FS (BYTES) HANDSHAKE
+
 	lissandra_thread_t l_thread;
 	l_thread_create(&l_thread, &correr_servidor_memoria, NULL);
 
