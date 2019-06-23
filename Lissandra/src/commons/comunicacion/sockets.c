20dad853567cc28cf4ed602b7136d7e9601239c6
PabloPaglilla <pablopaglilla16@gmail.com>
2019-06-23 02:03:55 -0300

    Agregar función obtener_ip_propio

--- Lissandra/src/commons/comunicacion/sockets.c
+++ Lissandra/src/commons/comunicacion/sockets.c
@@ -1,20 +1,23 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/select.h>
+#include <ifaddrs.h>
+#include <commons/string.h>
 
 #include "sockets.h"
 
-
-int get_local_addrinfo(const char* port, struct addrinfo* hints, struct addrinfo** server_info) {
+int get_local_addrinfo(const char* port, struct addrinfo* hints,
+		struct addrinfo** server_info) {
 
 	// Wrapper para getaddrinfo cuando se busca la addrinfo propia
 
 	int ret;
-	if((ret = getaddrinfo(NULL, port, hints, server_info)) != 0) {
+	if ((ret = getaddrinfo(NULL, port, hints, server_info)) != 0) {
 		return -1;
 	}
 	return 0;
@@ -25,13 +28,12 @@ int get_socket(const struct addrinfo* addr, int flag) {
 	// Recibe un addrinfo y retorna el file descriptor de un socket
 	// asociado a ella
 
-	int socket_fd = socket(addr->ai_family,
-		addr->ai_socktype,
-		addr->ai_protocol);
+	int socket_fd = socket(addr->ai_family, addr->ai_socktype,
+			addr->ai_protocol);
 	if (socket_fd == -1) {
 		return -1;
 	}
-	if(flag == FLAG_NON_BLOCK && (fcntl(socket_fd, F_SETFL, O_NONBLOCK) < 0)) {
+	if (flag == FLAG_NON_BLOCK && (fcntl(socket_fd, F_SETFL, O_NONBLOCK) < 0)) {
 		close(socket_fd);
 		return -1;
 	}
@@ -45,11 +47,11 @@ int get_binded_socket(const struct addrinfo* possible_addrinfo, int flag) {
 
 	int socket_fd;
 
-	if((socket_fd = get_socket(possible_addrinfo, flag)) == -1) {
+	if ((socket_fd = get_socket(possible_addrinfo, flag)) == -1) {
 		return -1;
 	}
 
-	if(bind(socket_fd, possible_addrinfo->ai_addr,
+	if (bind(socket_fd, possible_addrinfo->ai_addr,
 			possible_addrinfo->ai_addrlen) == -1) {
 		close(socket_fd);
 		return -1;
@@ -65,14 +67,13 @@ int get_connected_socket(const struct addrinfo* possible_addrinfo, int flag) {
 
 	int socket_fd;
 
-	if((socket_fd = get_socket(possible_addrinfo, flag)) == -1) {
+	if ((socket_fd = get_socket(possible_addrinfo, flag)) == -1) {
 		return -1;
 	}
 
-	if(connect(socket_fd,
-			possible_addrinfo->ai_addr,
+	if (connect(socket_fd, possible_addrinfo->ai_addr,
 			possible_addrinfo->ai_addrlen) == -1 &&
-			errno != EINPROGRESS) {
+	errno != EINPROGRESS) {
 
 		close(socket_fd);
 		return -1;
@@ -82,7 +83,7 @@ int get_connected_socket(const struct addrinfo* possible_addrinfo, int flag) {
 }
 
 int loop_addrinfo_list(struct addrinfo* linked_list,
-		int (*socket_builder) (const struct addrinfo*, int), int flag) {
+		int (*socket_builder)(const struct addrinfo*, int), int flag) {
 
 	// Recibe una lista enlazada resultado de getaddrinfo y un puntero
 	// a una función. La función debe tomar un addrinfo y devolver
@@ -94,8 +95,8 @@ int loop_addrinfo_list(struct addrinfo* linked_list,
 	struct addrinfo* p;
 	int socket_fd;
 
-	for(p = linked_list; p != NULL; p = p->ai_next) {
-		if((socket_fd = socket_builder(p, flag)) != -1) {
+	for (p = linked_list; p != NULL; p = p->ai_next) {
+		if ((socket_fd = socket_builder(p, flag)) != -1) {
 			return socket_fd;
 		}
 	}
@@ -117,18 +118,19 @@ int create_socket_server(const char* port, int backlog) {
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	if(get_local_addrinfo(port, &hints, &server_info) != 0){
+	if (get_local_addrinfo(port, &hints, &server_info) != 0) {
 		return -1;
 	}
 
-	if((socket_fd = loop_addrinfo_list(server_info, &get_binded_socket, FLAG_NONE)) == -1) {
+	if ((socket_fd = loop_addrinfo_list(server_info, &get_binded_socket,
+			FLAG_NONE)) == -1) {
 		freeaddrinfo(server_info);
 		return -1;
 	}
 
 	freeaddrinfo(server_info);
 
-	if(listen(socket_fd, backlog) == -1) {
+	if (listen(socket_fd, backlog) == -1) {
 		close(socket_fd);
 		return -1;
 	}
@@ -149,11 +151,12 @@ int create_socket_client(const char* host, const char* port, int flag) {
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if((ret = getaddrinfo(host, port, &hints, &server_info)) != 0) {
+	if ((ret = getaddrinfo(host, port, &hints, &server_info)) != 0) {
 		return -1;
 	}
 
-	if((socket_fd = loop_addrinfo_list(server_info, &get_connected_socket, flag)) == -1) {
+	if ((socket_fd = loop_addrinfo_list(server_info, &get_connected_socket,
+			flag)) == -1) {
 		freeaddrinfo(server_info);
 		return -1;
 	}
@@ -178,26 +181,53 @@ int wait_for_connection(int socket_fd, int timeout_ms) {
 	FD_ZERO(&rfs);
 	FD_SET(socket_fd, &rfs);
 
-	if((ret = select(socket_fd + 1, NULL, &rfs, NULL, &tv)) <= 0) {
+	if ((ret = select(socket_fd + 1, NULL, &rfs, NULL, &tv)) <= 0) {
 		// Hubo un error en select o se llegó al timeout
 		close(socket_fd);
 		return -1;
 	}
-	if((ret = getsockopt(socket_fd, SOL_SOCKET, SO_ERROR, &sock_opt, &sock_len)) < 0
-			|| sock_opt) {
+	if ((ret = getsockopt(socket_fd, SOL_SOCKET, SO_ERROR, &sock_opt, &sock_len))
+			< 0 || sock_opt) {
 		// Puede haber fallado tanto getsockopt como el connect
 		close(socket_fd);
 		return -1;
 	}
 	// Si getpeername retorna cero, el socket está conectado
-	return getpeername(socket_fd, (struct sockaddr*)&peer_addr, &sock_len);
+	return getpeername(socket_fd, (struct sockaddr*) &peer_addr, &sock_len);
 }
 
 int socket_set_blocking(int socket_fd) {
 	int flags = fcntl(socket_fd, F_GETFL);
-	if(flags == -1) {
+	if (flags == -1) {
 		return -1;
 	}
 	flags &= ~O_NONBLOCK;
 	return fcntl(socket_fd, F_SETFL, flags) != -1 ? 0 : -1;
+}
+
+int get_ip_propia(char *resultado, int len_resultado) {
+	struct ifaddrs *id;
+	struct sockaddr_in *sa;
+	char *addr;
+
+	if(len_resultado < 16) {
+		return -1;
+	}
+
+	if (getifaddrs(&id) == -1) {
+		return -1;
+	}
+
+	for (struct ifaddrs *ifa = id; ifa; ifa = ifa->ifa_next) {
+		if (ifa->ifa_addr->sa_family == AF_INET) {
+			sa = (struct sockaddr_in *) ifa->ifa_addr;
+			addr = inet_ntoa(sa->sin_addr);
+			if(!string_starts_with(addr, "127")) {
+				strcpy(resultado, addr);
+			}
+		}
+	}
+
+	freeifaddrs(id);
+	return 0;
 }
--- Lissandra/src/commons/comunicacion/sockets.h
+++ Lissandra/src/commons/comunicacion/sockets.h
@@ -1,6 +1,8 @@
 #ifndef SOCKETS_H_
 #define SOCKETS_H_
 
+#include <stdint.h>
+
 enum flags { FLAG_NONE, FLAG_NON_BLOCK };
 
 int create_socket_server(const char*, int);
@@ -10,5 +12,7 @@ int create_socket_client(const char*, const char *, int);
 int wait_for_connection(int, int);
 
 int socket_set_blocking(int);
+
+int get_ip_propia(char*, int);
 
 #endif
