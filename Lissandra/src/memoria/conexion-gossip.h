63287d639a4e68d566b22f86582805a18a5d0530
nicomelnyk <nicolasmelnyk@hotmail.com>
2019-06-24 02:53:09 -0300

    Se crean archivos para poder conectar memorias en el gossip

---
+++ Lissandra/src/memoria/conexion-gossip.h
@@ -0,0 +1,10 @@
+#ifndef LISSANDRA_SRC_MEMORIA_CONEXION_GOSSIP_H_
+#define LISSANDRA_SRC_MEMORIA_CONEXION_GOSSIP_H_
+
+int conectar_gossip();
+
+int desconectar_gossip();
+
+int realizar_gossip(uint8_t);
+
+#endif /* LISSANDRA_SRC_MEMORIA_CONEXION_GOSSIP_H_ */
